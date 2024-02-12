25377d3bec87895395d97da1a14b3a9f245f65d3
AndrewPhamCode <151807689+AndrewPhamCode@users.noreply.github.com>
2024-02-12 13:15:52 -0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("revealButton").addEventListener("click", function() {
-    var hiddenMessage = document.getElementById("hiddenMessage");
-    hiddenMessage.style.display = "block";
-    this.style.display = "none";
-});
